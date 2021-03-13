93c58e2a9bb2ef685c17e21bcefd2ef083b7fc01
ku-now <54778564+ku-now@users.noreply.github.com>
2021-03-13 22:06:03 +0900

    Add files via upload

--- decibel/script.js
+++ decibel/script.js
@@ -3,7 +3,7 @@ let Calc = () => {
 
   let val1 = parseFloat(document.getElementById('input1').value);
   let val2 = parseFloat(document.getElementById('input2').value);
-  let funcNum = parseInt(document.getElementById('func').value);
+  let funcNum = parseInt(document.getElementById('func1').value);
 
   switch (funcNum) {
     case 1:
@@ -16,9 +16,30 @@ let Calc = () => {
       value = 10 * Math.log10(Math.pow(10, val1 / 10) + Math.pow(10, val2 / 10));
       break;
     case 4:
-      value = 10 * Math.log10(Math.pow(10, val1 / 10) - Math.pow(10, val2 / 10));
+      val1 = Math.pow(10, val1 / 10);
+      val2 = Math.pow(10, val2 / 10);
+      value = ((val1 - val2) > 0) ? (10 * Math.log10(val1 - val2)) : 0;
       break;
   }
 
   document.getElementById('Result').innerHTML = `${value}dB`;
+};
+
+let Convert = () => {
+  let value = 0.0;
+
+  let val1 = parseFloat(document.getElementById('input3').value);
+  let val2 = parseFloat(document.getElementById('input4').value);
+  let funcNum = parseInt(document.getElementById('func2').value);
+
+  switch (funcNum) {
+    case 1:
+      value = Math.pow(10, val1 / 20);
+      document.getElementById('input4').value = value;
+      break;
+    case 2:
+      value = (val2 > 0) ? (20 * Math.log10(val2)) : 0;
+      document.getElementById('input3').value = value;
+      break;
+  }
 };
