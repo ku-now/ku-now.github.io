bc2dac970a93ead6229b04035ab3e1b316024bee
ku-now <54778564+ku-now@users.noreply.github.com>
2021-03-14 11:34:18 +0900

    Add files via upload

--- decibel/script.js
+++ decibel/script.js
@@ -1,24 +1,25 @@
 let Calc = () => {
   let value = 0.0;
 
+  const coef = parseInt(document.getElementById('form1').radio1.value);
   let val1 = parseFloat(document.getElementById('input1').value);
   let val2 = parseFloat(document.getElementById('input2').value);
   let funcNum = parseInt(document.getElementById('func1').value);
 
   switch (funcNum) {
     case 1:
-      value = 10 * Math.log10(Math.pow(10, val1 / 10) * Math.pow(10, val2 / 10));
+      value = coef * Math.log10(Math.pow(10, val1 / coef) * Math.pow(10, val2 / coef));
       break;
     case 2:
-      value = 10 * Math.log10(Math.pow(10, val1 / 10) / Math.pow(10, val2 / 10));
+      value = coef * Math.log10(Math.pow(10, val1 / coef) / Math.pow(10, val2 / coef));
       break;
     case 3:
-      value = 10 * Math.log10(Math.pow(10, val1 / 10) + Math.pow(10, val2 / 10));
+      value = coef * Math.log10(Math.pow(10, val1 / coef) + Math.pow(10, val2 / coef));
       break;
     case 4:
-      val1 = Math.pow(10, val1 / 10);
-      val2 = Math.pow(10, val2 / 10);
-      value = ((val1 - val2) > 0) ? (10 * Math.log10(val1 - val2)) : 0;
+      val1 = Math.pow(10, val1 / coef);
+      val2 = Math.pow(10, val2 / coef);
+      value = ((val1 - val2) > 0) ? (coef * Math.log10(val1 - val2)) : 0;
       break;
   }
 
@@ -28,17 +29,18 @@ let Calc = () => {
 let Convert = () => {
   let value = 0.0;
 
+  const coef = parseInt(document.getElementById('form1').radio1.value);
   let val1 = parseFloat(document.getElementById('input3').value);
   let val2 = parseFloat(document.getElementById('input4').value);
   let funcNum = parseInt(document.getElementById('func2').value);
 
   switch (funcNum) {
     case 1:
-      value = Math.pow(10, val1 / 20);
+      value = Math.pow(10, val1 / coef);
       document.getElementById('input4').value = value;
       break;
     case 2:
-      value = (val2 > 0) ? (20 * Math.log10(val2)) : 0;
+      value = (val2 > 0) ? (coef * Math.log10(val2)) : 0;
       document.getElementById('input3').value = value;
       break;
   }
